631c065bd01a5cded0a9d2dfb88b9a0203aced04
timothyrmallett <timothyrmallett@gmail.com>
2018-03-17 16:35:43 +1000

    messed with canvas

--- src/pages/tutorial/tutorial.ts
+++ src/pages/tutorial/tutorial.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild} from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 
@@ -9,11 +9,43 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class TutorialPage {
 
+	@ViewChild('canvas') canvasEl: ElementRef;
+
+	private _CANVAS: any;
+	private _CONTEXT: any;
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
+
   ionViewDidLoad() {
     console.log('ionViewDidLoad TutorialPage');
+
+    this._CANVAS = this.canvasEl.nativeElement;
+    this._CANVAS.width = 200;
+    this._CANVAS.height = 200;
+
+
+    this.initialiseCanvas();
+    //this.drawCircle();
+
   }
+
+  initialiseCanvas() : void
+   {
+      if(this._CANVAS.getContext)
+      {
+         this.setupCanvas();
+      }
+   }
+
+   setupCanvas() : void
+   {
+      this._CONTEXT = this._CANVAS.getContext('2d');
+      this._CONTEXT.fillStyle = "#3e3e3e";
+      this._CONTEXT.fillRect(0, 0, 500, 500);
+   }
+
+
 
 }
